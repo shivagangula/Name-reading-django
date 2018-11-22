5b853ffb7469af07c8a0f7de0cefeea8d7568143
Shiva Gangula <ashiva44@gmail.com>
2018-11-22 22:53:50 +0530

    privous user check add in signup and signin

--- NameReading/user/views.py
+++ NameReading/user/views.py
@@ -17,27 +17,45 @@ def signin(request):
 
 
 def signup_success(request):
-        if request.GET['mobile_number'] != None:
-            username = request.GET['username']
-            password = request.GET['password']
-            mobile_number = request.GET['mobile_number']
-            email = request.GET['email']
-            list = [username, password, mobile_number, email]
-            if "" in list:
-                send = {'info': 'enter all detailes properly'}
-                return render(request, 'user/signup.html', send)
-            else:
-                md5_hash = convertmd5(email)
-                user = user_singup_model(
-                    username=username,
-                    password=password,
-                    mobile_number=mobile_number,
-                    email=email,
-                    md5_hash=md5_hash
+
+    username = request.GET['username']
+    password = request.GET['password']
+    mobile_number = request.GET['mobile_number']
+    email = request.GET['email']
+    signup_details = [username, password, mobile_number, email]
+
+    # privous user or not
+    verify = [ email, password ]
+    md5_hash = convertmd5(email)
+    cursor = connection.cursor()
+    query = """
+                    SELECT md5_hash FROM user_user_singup_model
+                    WHERE email= '{0}' AND password = '{1}';
+                    """.format(email, password)
+    cursor.execute(query)
+    row = cursor.fetchall()
+
+    try:
+        if "" in signup_details:
+          send = {'info': 'enter all detailes properly'}
+          return render(request, 'user/signup.html', send)
+
+        if md5_hash in row[ 0 ]:
+            send = {'info': "user already exist"}
+            return render(request, 'user/signup.html', send)
+    except:
+        md5_hash = convertmd5(email)
+        user = user_singup_model(
+        username=username,
+        password=password,
+        mobile_number=mobile_number,
+        email=email,
+        md5_hash=md5_hash
                 )
-                user.save()
-                send = {'info': 'registration success'}
-                return render(request, 'user/signup_success.html', send)
+        user.save()
+        send = {'info': 'registration success'}
+        return render(request, 'user/signup_success.html', send)
+
 
 def signin_success(request):
         email = request.GET['email']
@@ -52,14 +70,15 @@ def signin_success(request):
         cursor.execute(query)
         row = cursor.fetchall()
         print(row)
-        if "" in list1:
+
+        try:
+         if "" in list1:
             send = {'info': "type correctly"}
             return render(request, 'user/signin_success.html', send)
 
-        if md5_hash in row[0]:
+         if md5_hash in row[0]:
             send = {'info': "user is there"}
             return render(request, 'user/signin_success.html', send)
-
-        if md5_hash not in row[0]:
+        except:
             send = {'info': "user is not there"}
-            return render(request, 'user/signin_success.html', send)
+            return render(request, 'user/signin.html', send)
