7f5bee5a9b24b72a1b6ad79556e93c90df9cbf7d
Shiva Gangula <ashiva44@gmail.com>
2018-11-25 12:27:02 +0530

    24/11/2018 12:26pm

--- NameReading/user/Opreations/signin.py
+++ NameReading/user/Opreations/signin.py
@@ -1,7 +1,17 @@
 from django.db import connection
-
 from user.Opreations.md5_encript import convertmd5
 
+
+def currentUser(email):
+    md5_hash = convertmd5(email)
+    cursor = connection.cursor()
+    query = """
+                        select username from user_user_singup_model 
+                         where email = '{0}' ;
+                        """.format(email)
+    cursor.execute(query)
+    row = cursor.fetchall()
+    return row[0][0]
 
 def SignInQuery(email, password):
     md5_hash = convertmd5(email)
@@ -14,6 +24,7 @@ def SignInQuery(email, password):
     cursor.execute(query)
     row = cursor.fetchall()
     print(row)
+    cuser = currentUser(email)
     try:
           if email == "":
               return {'info':'enter email adress'}
@@ -22,7 +33,7 @@ def SignInQuery(email, password):
           if "" in list1:
                 return {'info': "type correctly"}
           if md5_hash in row[ 0 ]:
-                return {'info': "success"}
+                return {'info': "success",'username': cuser}
 
     except:
           return {'info': "user is not there"}
--- NameReading/user/views.py
+++ NameReading/user/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-
-from user.Opreations.md5_encript import convertmd5
 from user.Opreations.signin import SignInQuery
 from user.Opreations.signup import signup_qurey
-from .models import user_singup_model
-from django.db import connection
+
 
 def user(request):
     return render(request, 'user/signin.html')
@@ -19,28 +16,59 @@ def signin(request):
 
 
 def signup_success(request):
-
+    #getting requests from signup form
     username = request.GET['username']
     password = request.GET['password']
     mobile_number = request.GET['mobile_number']
     email = request.GET['email']
 
+
+
+
+    #calling singup_qurey function from operation.signup package
+    # it will return resonable output strings in the form of python
+    #dictionary for furtur purpose
     recived_data = signup_qurey(username,password,mobile_number,email)
 
+
+
+
+    #after recived dictionary.check is 'success' string is their
+    #or not in recived dictionar if thier it redirect
+    #login page
     if recived_data['info'] == "success":
       send = {'info':'your registration succesfull'}
       return render(request, 'user/signin.html', send)
-
+    #if recived dictionay have no 'success' string it return
+    #what problem is occured then showed current signup page
     else:
         return render(request, 'user/signup.html', recived_data)
 
 
 def signin_success(request):
+
+
+
+    # taking email and password from user signin form
         email = request.GET['email']
         password = request.GET['password']
+
+
+
+
+
+    #calling signinquery() function from oparation package
+    #it work same as singupquery() function
         recived_data = SignInQuery(email, password)
+
+
+
+
+
+
+    #if success is their in recived_data it finally shows original page with user name
         if recived_data['info'] == 'success':
-            send = {'info':'success'}
-            return render(request, 'nr_game/index.html' ,send)
+            return render(request, 'nr_game/index.html' ,recived_data)
+    #else it shows what problem is occured in current signin page
         else:
             return render(request, 'user/signin.html', recived_data)
